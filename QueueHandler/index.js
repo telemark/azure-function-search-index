a56f4d91b95d9ec33312ffb273822ccd0f0da306
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-10-25 15:11:20 +0200

    Adds bits and pieces to make it work with queue

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -4,9 +4,11 @@ const deleteIndex = require('../lib/delete-index')
 module.exports = async function (context, mySbMsg) {
   const doAction = mySbMsg.action === 'delete' ? deleteIndex : mySbMsg.action === 'add' ? addIndex : false
   if (doAction !== false) {
-    const result = doAction(mySbMsg.payload)
+    context.log(`${mySbMsg.id} - ${mySbMsg.action}`)
+    const result = await doAction(context, mySbMsg.payload)
     context.log(result)
   } else {
     context.log(`Unknown action - ${mySbMsg.action}`)
   }
+  context.log('Finished')
 }
