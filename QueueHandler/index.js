2e3b3f862cfd85dc18682b60523e703c1a8f9678
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-11-23 06:56:28 +0100

    Adds stringify for result log

--- QueueHandler/index.js
+++ QueueHandler/index.js
@@ -3,6 +3,6 @@ const addIndex = require('../lib/add-index')
 module.exports = async function (context, mySbMsg) {
   context.log(`${mySbMsg.id} - start`)
   const result = await addIndex(context, mySbMsg.payload)
-  context.log(`${mySbMsg.id} - ${result}`)
+  context.log(`${mySbMsg.id} - ${JSON.stringify(result)}`)
   context.log('Finished')
 }
